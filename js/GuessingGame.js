29bc68c9fa9a4bdff41e547ce023b2b821ca670f
Darryn Wu <darryn8wu@gmail.com>
2017-05-31 19:37:18 -0400

    Finished Beta Game

--- js/GuessingGame.js
+++ js/GuessingGame.js
@@ -14,13 +14,18 @@ Game.prototype.playersGuessSubmission = function(guess){
 
 Game.prototype.checkGuess = function(){
   if(this.playersGuess===this.winningNumber) {
+    $('#hint, #submit').prop("disabled", true);
+    $('#subtitle').text("Press the Reset button to play again!")
     return "You Win!"
   } else {
     if(this.pastGuesses.indexOf(this.playersGuess) > -1){
       return "You have already guessed that number.";
     } else {
       this.pastGuesses.push(this.playersGuess);
+      $('#guess-list li:nth-child('+ this.pastGuesses.length +')').text(this.playersGuess);
       if(this.pastGuesses.length === 5) {
+        $('#hint, #submit').prop("disabled",true);
+        $('#subtitle').text("Press the Reset button to play again!");
         return "You Lose.";
       } else {
         var diff = this.difference();
@@ -57,10 +62,8 @@ function newGame(){
 function shuffle(array){
 
   var m = array.length, t, i;
-
   // While there remain elements to shuffle…
   while (m) {
-
     // Pick a remaining element…
     i = Math.floor(Math.random() * m--);
 
@@ -71,5 +74,42 @@ function shuffle(array){
   }
   return array;
 }
+
+function makeAGuess(game) {
+
+    var guess = $("#player-input").val();
+    $("#player-input").val("");
+    var output = game.playersGuessSubmission(parseInt(guess,10));
+    $("#title").text(output);
+    console.log("this is your Guess: "+guess);
+}
+
+$(document).ready(function(){
+  var game = new Game();
+
+$('#submit').click(function(e){
+    makeAGuess(game);
+  });
+
+$('#player-input').keypress(function(event) {
+    if ( event.which == 13 ) {
+      makeAGuess(game);
+    }
+  });
+
+$('#hint').click(function() {
+        var hints = game.provideHint();
+        $('#title').text('The winning number is '+hints[0]+', '+hints[1]+', or '+hints[2]);
+  });
+
+$('#reset').click(function() {
+        game = newGame();
+        $('#title').html( "<font color='red'>P</font><font color='green'>l</font><font color='yellow'>a</font><font color='blue'>y </font><font color='green'>t</font><font color='red'>h</font><font color='brown'>e </font><font color='blue'>G</font><font color='purple'>u</font><font color='silver'>e</font><font color='black'>s</font><font color='red'>s</font><font color='black'>i</font><font color='red'>n</font><font color='red'>g </font><font color='blue'>G</font><font color='green'>a</font><font color='brown'>m</font><font color='red'>e</font><font color='red'>!</font>" );
+        $('#subtitle').text( "Guess a number between 1-100!" );
+        $('.guess').text( "-" );
+        $('#hint, #submit').prop("disabled",false);
+  });
+
+});
 
 
